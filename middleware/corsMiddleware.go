253d7b002441489b14934cc9f6c4408b3b066df7
Joker <infinitedemon@foxmail.com>
2023-11-23 10:37:34 +0800

    feat: 添加依赖

--- middleware/corsMiddleware.go
+++ middleware/corsMiddleware.go
@@ -52,7 +52,7 @@ func (m *CorsMiddleware) Handler() http.Handler {
 // setHeader 设置响应头
 func setHeader(w http.ResponseWriter) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token, X-Token, X-User-Id, X-AccessKey-For")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
 	w.Header().Set("Access-Control-Expose-Headers", "Content-Length, Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Headers")
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
