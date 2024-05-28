363cfa0e25cd0ca166f113b19911852e98dfbd14
Joker <infinitedemon@foxmail.com>
2024-05-28 18:45:30 +0800

    feat: 添加rbac的多参数适配

--- rbac/rbac.go
+++ rbac/rbac.go
@@ -1,13 +1,33 @@
 package rbac
 
 import (
+	"errors"
 	"github.com/casbin/casbin/v2"
 	gormadapter "github.com/casbin/gorm-adapter/v3"
 	"gorm.io/gorm"
 )
 
-func InitRbac(path string, db *gorm.DB) (*casbin.Enforcer, error) {
-	adapter, err := gormadapter.NewAdapterByDB(db)
+const (
+	defaultDatabaseName = "casbin"
+	defaultTableName    = "casbin_rule"
+)
+
+func InitRbac(path string, db *gorm.DB, v ...any) (*casbin.Enforcer, error) {
+	prefix := ""
+	tableName := ""
+	if len(v) == 0 {
+		prefix = ""
+		tableName = defaultTableName
+	} else if len(v) == 1 {
+		prefix = v[0].(string)
+		tableName = defaultTableName
+	} else if len(v) == 2 {
+		prefix = v[0].(string)
+		tableName = v[1].(string)
+	} else {
+		return nil, errors.New("wrong parameters")
+	}
+	adapter, err := gormadapter.NewAdapterByDBUseTableName(db, prefix, tableName)
 	if err != nil {
 		return nil, err
 	}
