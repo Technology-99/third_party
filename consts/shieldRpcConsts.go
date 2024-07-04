eb57b19167915fdb9de14f21128580024dcb0eea
Joker <infinitedemon@foxmail.com>
2024-07-05 00:18:12 +0800

    feat: 添加JOBS_RPC_SERVICE_NAME

--- consts/shieldRpcConsts.go
+++ consts/shieldRpcConsts.go
@@ -1,5 +1,6 @@
 package consts
 
 const (
-	SHIELD_RPC_SERVICE_NAME = "shield.ShieldRpcService:"
+	SHIELD_RPC_SERVICE_NAME = "shield.ShieldRpcService"
+	JOBS_RPC_SERVICE_NAME   = "jobs.JobsRpcService"
 )
