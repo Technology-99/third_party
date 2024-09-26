00e1084f8e1a4ed8248c9a6c1ae105b3b6e73c69
Joker <infinitedemon@foxmail.com>
2024-09-26 21:29:28 +0800

    feat: 添加mysql的原生连接的支持与否

--- database/connect.go
+++ database/connect.go
@@ -12,6 +12,7 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	gomysql "github.com/go-sql-driver/mysql"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -22,19 +23,20 @@ import (
 )
 
 type DbConfig struct {
-	Host        string
-	Port        uint
-	DbName      string
-	User        string
-	Password    string
-	Charset     string
-	MaxIdle     int
-	MaxOpen     int
-	LogMode     bool
-	Loc         string
-	MaxLifetime int64
-	TablePrefix string
-	Debug       bool
+	Host                 string
+	Port                 uint
+	DbName               string
+	User                 string
+	Password             string
+	Charset              string
+	MaxIdle              int
+	MaxOpen              int
+	LogMode              bool
+	Loc                  string
+	MaxLifetime          int64
+	TablePrefix          string
+	Debug                bool
+	AllowNativePasswords bool
 }
 
 func NewDbClient(c *DbConfig) (*gorm.DB, error) {
@@ -59,13 +61,15 @@ func NewDbClient(c *DbConfig) (*gorm.DB, error) {
 	}
 
 	db, err := gorm.Open(mysql.New(mysql.Config{
-		DSN:                       dsn,   // DSN data source name
+		DSN: dsn, // DSN data source name
+		DSNConfig: &gomysql.Config{
+			AllowNativePasswords: c.AllowNativePasswords,
+		}, // 额外的配置
 		DefaultStringSize:         255,   // string 类型字段的默认长度
 		DisableDatetimePrecision:  true,  // 禁用 datetime 精度，MySQL 5.6 之前的数据库不支持
 		DontSupportRenameIndex:    true,  // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
 		DontSupportRenameColumn:   true,  // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
 		SkipInitializeWithVersion: false, // 根据当前 MySQL 版本自动配置
-
 	}), &gorm.Config{
 		Logger: newLogger,
 		NamingStrategy: schema.NamingStrategy{
