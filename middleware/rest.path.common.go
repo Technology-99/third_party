b9a6b6c98f4ebe682d621f60edc4e65f0e2d8ba1
howard <howard.huang@itech99.com>
2024-12-01 19:27:32 +0800

    优化rest.paht.common下的path中间件

--- middleware/rest.path.common.go
+++ middleware/rest.path.common.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"context"
+	"github.com/Technology-99/third_party/sony"
 	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/rest/httpx"
 	"net/http"
@@ -21,10 +22,17 @@ func (m *PathHttpInterceptorMiddleware) Handle(next http.HandlerFunc) http.Handl
 		ctx = context.WithValue(ctx, "RequestURI", r.RequestURI)
 		fullAddr := httpx.GetRemoteAddr(r)
 		fullAddrAndPort := strings.Split(fullAddr, ":")
-		logx.Infof("fullAddrAndPort: %v", fullAddrAndPort)
-		logx.Infof("client ip : %s", fullAddrAndPort[0])
-		//logx.Infof("client port : %s", fullAddrAndPort[1])
 		ctx = context.WithValue(ctx, "clientIp", fullAddrAndPort[0])
+		logx.Infof("client ip : %s", fullAddrAndPort[0])
+		logx.Infof("requestID: %v", ctx.Value("RequestID"))
+
+		requestID := ""
+		if ctx.Value("RequestID") == nil {
+			requestID = sony.NextId()
+		} else {
+			requestID = ctx.Value("RequestID").(string)
+		}
+		ctx = context.WithValue(ctx, "RequestID", requestID)
 		//ctx = context.WithValue(ctx, "clientPort", fullAddrAndPort[1])
 		r = r.WithContext(ctx)
 		next(w, r)
