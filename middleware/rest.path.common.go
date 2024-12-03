3b71f354d6654538b8f2c8ae45f256ca1ea3184e
howard <howard.huang@itech99.com>
2024-12-04 00:52:42 +0800

    feat: 添加自动生成accessKey和相关数据的工具函数

--- middleware/rest.path.common.go
+++ middleware/rest.path.common.go
@@ -6,6 +6,7 @@ import (
 	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/rest/httpx"
 	"net/http"
+	"regexp"
 	"strings"
 )
 
@@ -21,10 +22,38 @@ func (m *PathHttpInterceptorMiddleware) Handle(next http.HandlerFunc) http.Handl
 		ctx := context.WithValue(r.Context(), CtxFullMethod, r.URL.Path)
 		ctx = context.WithValue(ctx, CtxRequestURI, r.RequestURI)
 		fullAddr := httpx.GetRemoteAddr(r)
-		logx.Infof("fullAddr: %s", fullAddr)
-		fullAddrAndPort := strings.Split(fullAddr, ":")
-		ctx = context.WithValue(ctx, CtxClientIp, fullAddrAndPort[0])
-		logx.Infof("client ip : %s", fullAddrAndPort[0])
+		// 定义正则表达式
+		regex := `^\[?([a-fA-F0-9:.%]+)\]?(?::([0-9]+))?$`
+		re := regexp.MustCompile(regex)
+		// 去除首尾空格
+		fullAddr = strings.TrimSpace(fullAddr)
+		// 匹配输入字符串
+		matches := re.FindStringSubmatch(fullAddr)
+		// 匹配输入字符串
+		if len(matches) != 3 {
+			fullAddrAndPort := strings.Split(fullAddr, ":")
+			if len(fullAddrAndPort) == 1 {
+				logx.Infof("client ip: %s", fullAddrAndPort[0])
+				ctx = context.WithValue(ctx, CtxClientIp, fullAddrAndPort[0])
+			} else if len(fullAddrAndPort) == 2 {
+				logx.Infof("client ip: %s", fullAddrAndPort[0])
+				logx.Infof("client port: %s", fullAddrAndPort[1])
+				ctx = context.WithValue(ctx, CtxClientIp, fullAddrAndPort[0])
+				ctx = context.WithValue(ctx, CtxClientPort, fullAddrAndPort[1])
+			} else {
+				logx.Errorf("fullAddr: %s, fullAddrAndPort: %v", fullAddr, fullAddrAndPort)
+			}
+		} else {
+			clientIp := matches[1]
+			clientPort := matches[2]
+			ctx = context.WithValue(ctx, CtxClientPort, clientPort)
+			if matches[1] == "::1" {
+				clientIp = "127.0.0.1"
+			}
+			ctx = context.WithValue(ctx, CtxClientIp, clientIp)
+			logx.Infof("client ip: %s", clientIp)
+			logx.Infof("client port: %s", clientPort)
+		}
 
 		requestID := ""
 		if ctx.Value(CtxRequestID) == nil {
