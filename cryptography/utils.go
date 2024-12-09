4730af5c1eb0df66a14bf57a49c56c9f1653c59e
howard <howard.huang@itech99.com>
2024-12-09 11:27:02 +0800

    feat: 更新path中间件中的requestId的处理

--- cryptography/utils.go
+++ cryptography/utils.go
@@ -5,6 +5,35 @@ import (
 	"encoding/hex"
 )
 
+func GenAccessAndSecret() (AccessKey, AccessSecret string, err error) {
+	AccessKey = GenerateTicket("", 16)
+	//创建secret
+	hash := sha1.New()
+	hash.Write([]byte(AccessKey))
+	sum := hash.Sum(nil)
+	sign := hex.EncodeToString(sum)
+	AccessSecret = ToUp(sign)
+	return AccessKey, AccessSecret, nil
+}
+
+func GenTenantRsa() (AccessPriKey, AccessPubKey, RefreshPriKey, RefreshPubKey, EncryptionPriKey, EncryptionPubKey []byte, err error) {
+	//生成rsa
+	AccessPriKey, AccessPubKey, err = GenRsaKey()
+	if err != nil {
+		return nil, nil, nil, nil, nil, nil, err
+	}
+	RefreshPriKey, RefreshPubKey, err = GenRsaKey()
+	if err != nil {
+		return nil, nil, nil, nil, nil, nil, err
+	}
+	EncryptionPriKey, EncryptionPubKey, err = GenRsaKey()
+	if err != nil {
+		return nil, nil, nil, nil, nil, nil, err
+	}
+
+	return AccessPriKey, AccessPubKey, RefreshPriKey, RefreshPubKey, EncryptionPriKey, EncryptionPubKey, nil
+}
+
 func GenAccessAndPwdAndRsa() (AccessKey, AccessSecret string, AccessPriKey, AccessPubKey, RefreshPriKey, RefreshPubKey []byte, err error) {
 	AccessKey = GenerateTicket("", 16)
 
