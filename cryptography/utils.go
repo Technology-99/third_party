52e65f3048fc2d0a98d213613f6d12e1bd4a9aad
howard <howard.huang@itech99.com>
2025-01-13 20:31:29 +0800

    feat: 更新生成base密码功能

--- cryptography/utils.go
+++ cryptography/utils.go
@@ -5,6 +5,13 @@ import (
 	"encoding/hex"
 )
 
+func GenPassword(pwd string) (basePwd, key, salt string) {
+	key = RandSalt(32)
+	salt = RandSalt(16)
+	_, basePwd = AesEncryptByCTR(pwd+salt, key)
+	return basePwd, key, salt
+}
+
 func GenAccessAndSecret() (AccessKey, AccessSecret string, err error) {
 	AccessKey = GenerateTicket("", 16)
 	//创建secret
